fc241fe4a11dba3ad1e88d3b6154d35445c063c8
llch <aceliuchanghong@gmail.com>
2025-05-17 21:34:29 +0800

    fix: 靠,原来不能取那个名字

--- new_docling.py
+++ new_docling.py
@@ -3,7 +3,7 @@ from docling.document_converter import DocumentConverter
 """
 模型下载:docling-tools models download
 export DOCLING_ARTIFACTS_PATH="/root/.cache/docling/models"
-python docling.py
+python new_docling.py
 """
 source = "no_git_oic/test_files/流式细胞制备方案.pdf"
 converter = DocumentConverter()
