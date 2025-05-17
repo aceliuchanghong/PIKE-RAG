1a1ce05d77b5f6c00e4b065c2e86d760ef2e4fd9
llch <aceliuchanghong@gmail.com>
2025-05-17 21:25:43 +0800

    feat: test docling

---
+++ docling.py
@@ -0,0 +1,11 @@
+from docling.document_converter import DocumentConverter
+
+"""
+模型下载:docling-tools models download
+export DOCLING_ARTIFACTS_PATH="/root/.cache/docling/models"
+python docling.py
+"""
+source = "no_git_oic/test_files/流式细胞制备方案.pdf"
+converter = DocumentConverter()
+result = converter.convert(source)
+print(result.document.export_to_markdown())
